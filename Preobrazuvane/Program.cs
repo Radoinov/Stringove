5bce56b9fb212a6e2978b4421f1b4e7dcf191169
Radoinov <radoinov08@gmail.com>
2024-03-19 21:58:13 +0200

    Domashna

---
+++ Preobrazuvane/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Preobrazuvane
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int baseNumber = int.Parse(Console.ReadLine());
+            int num = int.Parse(Console.ReadLine());
+
+            
+            string result = "";
+            while (num > 0)
+            {
+                int remainder = num % baseNumber;
+                result = remainder.ToString() + result;
+                num /= baseNumber;
+            }
+
+            
+            Console.WriteLine(result);
+        }
+    }
+}
